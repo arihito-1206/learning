802d46af473d7d6a98db27ffab7f830b315af276
arihito-1206 <you.get.2.burning@gmail.com>
2026-08-18 15:41:17 +0900

    :test_tube: Learn lambda-taking extension methods

--- dotnet/fluent-api/lambda-extension-method/Program.cs
+++ dotnet/fluent-api/lambda-extension-method/Program.cs
@@ -3,5 +3,17 @@
 // Console.WriteLine($"{doubleFunc(5)}");
 
 // Action 型を使った場合
+
 Action<int> doubleAction = x => Console.WriteLine($"{2 * x}");
 doubleAction(5);
+
+// 拡張メソッドの練習
+Console.WriteLine(5.Double());
+Console.WriteLine(5.Apply(x => x * x));
+
+internal static class IntExtensions
+{
+    public static int Double(this int i) => i * 2;
+
+    public static int Apply(this int i, Func<int, int> func) => func(i);
+}

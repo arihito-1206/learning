af17c78cb78a614f11fc5c75eca78ad58e2f56b5
arihito-1206 <you.get.2.burning@gmail.com>
2026-09-11 21:42:04 +0900

    🎉 Add minimal ActionBlock producer-consumer example

---
+++ dotnet/concurrency/actionblock-vs-channel/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks.Dataflow;
+
+var block = new ActionBlock<int>(val =>
+{
+    Console.WriteLine($"受け取った: {val}");
+});
+
+block.Post(1);
+block.Post(2);
+block.Post(3);
+
+block.Complete();
+await block.Completion;
